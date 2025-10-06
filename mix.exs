18ff2e95a819dbf36a94210f6c9aa33c306c36b0
Jerson Paniagua <diazpany@gmail.com>
2025-10-05 23:27:10 -0300

    add mix deps/aliases

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule EnsureConsistency.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      # <-- add this line
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -22,8 +25,25 @@ defmodule EnsureConsistency.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.15", only: :test},
+      {:ex_doc, "~> 0.30", only: [:dev], runtime: false},
+      {:inch_ex, "~> 2.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      ensure_consistency: [
+        "format --check-formatted",
+        "test",
+        "dialyzer",
+        "coveralls",
+        "credo --strict",
+        "inch",
+        "hex.audit"
+      ]
     ]
   end
 end
